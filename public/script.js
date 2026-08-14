6fdee813eafefb1b72e8e0455475365385b42ea0
Kiran <kiran24cs@rnsit.ac.in>
2026-08-15 00:12:33 +0530

    made sum small changes

--- public/script.js
+++ public/script.js
@@ -4,6 +4,7 @@ let userlogindiv=document.getElementById("userlogindiv");
 let shopregisterdiv=document.getElementById("shopregisterdiv");
 let shoplogindiv=document.getElementById("shoplogindiv");
 let locationdiv=document.getElementById("locationdiv");
+let findlocationbtn=document.getElementById("findlocationbtn");
 function initial(){
     locationdiv.style.display="none";
     userregisterdiv.style.display="none";
@@ -33,7 +34,53 @@ async function loaduserregister(){
 async function loadshopregister(){
     initial();
     shopregisterdiv.style.display="block";
+    findlocationbtn.innerText="Find Location";
 }
 function showlocationdiv(){
     locationdiv.style.display="block";
+}
+function findlocation() {
+    findlocationbtn.innerText="Locating...";
+    if (!navigator.geolocation) {
+        findlocation.innerText="Geolocation is not supported by your browser";
+        return;
+    }
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+
+            let latitude = position.coords.latitude;
+            let longitude = position.coords.longitude;
+            showlocationdiv();
+            document.getElementById("shopregisterlatitude").value = latitude;
+            document.getElementById("shopregisterlongitude").value = longitude;
+            findlocationbtn.innerText="Location Found";
+            
+        },
+        function(error) {
+
+            if (error.code === 1) {
+                alert("Location permission denied.");
+            } 
+            else if (error.code === 2) {
+                alert("Location unavailable.");
+            } 
+            else if (error.code === 3) {
+                alert("Location request timed out.");
+            } 
+            else {
+                alert("Unable to find your location.");
+            }
+
+        }
+    );
+}
+function locateonmap(){
+    let latitude=document.getElementById("shopregisterlatitude").value ;
+    let longitude=document.getElementById("shopregisterlongitude").value ;
+    if(!latitude || !longitude ){
+        alert("Enter longitude and latitude");
+        return;
+    }
+    let mapurl = `https://www.google.com/maps?q=${latitude},${longitude}`;
+    window.open(mapurl, "_blank");
 }
